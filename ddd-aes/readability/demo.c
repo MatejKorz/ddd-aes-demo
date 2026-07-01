a332b7a2cfc585d265cab8baf863cd158c7aa314
MatejKorz <korz.matej@gmail.com>
2026-07-01 11:35:03 -0400

    always little endian tweak

--- ddd-aes/readability/demo.c
+++ ddd-aes/readability/demo.c
@@ -9,13 +9,15 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <endian.h>
 
 #include "ddd-aes-ref-readability.h"
 
 #define BLOCK_SIZE 512
 #define BLOCK_COUNT 16
 
-typedef enum {
+typedef enum
+{
     MODE_NONE = 0,
     MODE_ENCRYPT = 1,
     MODE_DECRYPT = 2
@@ -32,11 +34,9 @@ int main(int argc, char* argv[])
 
     if (strcmp(argv[1], "-e") == 0) {
         mode = MODE_ENCRYPT;
-    }
-    else if (strcmp(argv[1], "-d") == 0) {
+    } else if (strcmp(argv[1], "-d") == 0) {
         mode = MODE_DECRYPT;
-    }
-    else {
+    } else {
         printf("Unknown option: %s\n", argv[1]);
         return 1;
     }
@@ -47,52 +47,54 @@ int main(int argc, char* argv[])
                                      0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF };
 
     uint8_t tweak[TWEAKSIZE_BYTES] = { 0 };
-    uint8_t input_buff[BLOCK_SIZE];
-    uint8_t output_buff[BLOCK_SIZE];
+    uint8_t input_buff[BLOCK_SIZE] = { 0 };
+    uint8_t output_buff[BLOCK_SIZE] = { 0 };
 
     int fd = open("input.bin", O_RDWR);
     if (fd < 0) {
-	perror("open input file");
-	return 1;
+        perror("open input file");
+        return 1;
     }
 
     for (uint64_t iv = 0; iv < BLOCK_COUNT; iv++) {
-	for (int i = 0; i < 8; i++) {
-	    tweak[i] = (iv >> (i * 8)) & 0xFF;
-	}
+        memset(tweak, 0, TWEAKSIZE_BYTES);
+        uint64_t iv_le = htole64(iv);
+        memcpy(tweak, &iv_le, sizeof(iv_le));
 
-	off_t offset = (off_t)(iv * BLOCK_SIZE);
-	ssize_t rbytes = pread(fd, input_buff, BLOCK_SIZE, offset);
-	if (rbytes < 0) {
-	    perror("pread");
-	    return 1;
-	}
-	if (rbytes == 0) {
-	    break;
-	}
+        off_t offset = (off_t)(iv * BLOCK_SIZE);
+        ssize_t rbytes = pread(fd, input_buff, BLOCK_SIZE, offset);
+        if (rbytes < 0) {
+            perror("pread");
+            return 1;
+        }
+        if (rbytes == 0) {
+            break;
+        }
 
-	int status;
-	switch (mode) {
-	    case MODE_ENCRYPT: 
-		status = ddd_aes_ref_read_encrypt(output_buff, input_buff, BLOCK_SIZE, key_k, key_l, tweak, TWEAKSIZE_BYTES);
-		break;
-	    case MODE_DECRYPT: 
-		status = ddd_aes_ref_read_decrypt(output_buff, input_buff, BLOCK_SIZE, key_k, key_l, tweak, TWEAKSIZE_BYTES);
-		break;
-	    default:
-		status = -1;
-	}
+        int status;
+        switch (mode) {
+            case MODE_ENCRYPT:
+                status =
+                  ddd_aes_ref_read_encrypt(output_buff, input_buff, BLOCK_SIZE, key_k, key_l, tweak, TWEAKSIZE_BYTES);
+                break;
+            case MODE_DECRYPT:
+                status =
+                  ddd_aes_ref_read_decrypt(output_buff, input_buff, BLOCK_SIZE, key_k, key_l, tweak, TWEAKSIZE_BYTES);
+                break;
+            default:
+                status = -1;
+        }
 
-	if (status < 0) {
+        if (status < 0) {
             fprintf(stderr, "ERROR: ddd_aes_ref_read failed, status: %d\n", status);
-	    return 1;
-    	}
+            return 1;
+        }
 
-	ssize_t wbytes = pwrite(fd, output_buff, (size_t)rbytes, offset);
-	if (wbytes != rbytes) {
-	    perror("pwrite");
-	    return 1;
-	}
+        ssize_t wbytes = pwrite(fd, output_buff, (size_t)rbytes, offset);
+        if (wbytes != rbytes) {
+            perror("pwrite");
+            return 1;
+        }
     }
 
     return 0;
